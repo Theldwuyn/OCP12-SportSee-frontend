9d534b5ec88847d929e82be08de86fff82e4b280
Guillaume <guillaumeargensse@hotmail.fr>
2024-12-20 18:07:40 +0100

    refactor, docs: modify activity component, add documentation

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,3 +1,7 @@
+/* -------------------------------------------------------------------------- */
+/*                                   IMPORT                                   */
+/* -------------------------------------------------------------------------- */
+
 import {
   Bar,
   BarChart,
@@ -17,19 +21,63 @@ import filterData from '../utils/filterData';
 // style
 import '../scss/components/activity.scss';
 
+/* -------------------------------------------------------------------------- */
+/*                              CUSTOM COMPONENT                              */
+/* -------------------------------------------------------------------------- */
+
 /* eslint-disable react/prop-types */
+/* the eslint is disable to avoid prop type warning, the props used by custom
+component are recharts props, and are implicitly passed to functions */
+
+/**
+ * Customize the tooltip by replacing the default value by {value}kg and
+ * {value}kCal
+ *
+ * This function is passed as value of content prop of Tooltip component
+ * @param {boolean} active
+ * @param {object} payload rechart object with all information from the chart
+ *
+ * payload[0] - kg bar information,
+ * payload[1] - calories bar information
+ * @returns
+ */
 function CustomToolTip({ active, payload }) {
   if (active && payload && payload.length) {
     return (
-      <div className="custom-tooltip">
-        <p className="label">{`${payload[0].value}${payload[0].unit}`}</p>
-        <p className="label">{`${payload[1].value}${payload[1].unit}`}</p>
+      <div className="custom-tooltip__barchart">
+        <p>{`${payload[0].value}${payload[0].unit}`}</p>
+        <p>{`${payload[1].value}${payload[1].unit}`}</p>
       </div>
     );
   }
 }
 
+/**
+ * Customize the X axis tick by replacing the default value
+ *
+ * Replace the date by the payload index + 1
+ *
+ * This function is passed as value of tick prop of XAxis component
+ * @param {number} x x position of the tick
+ * @param {number} y y position of the tick
+ * @param {object} payload rechart object with all axis information
+ * @returns
+ */
+function CustomAxisTick({ x, y, payload }) {
+  return (
+    <g>
+      <text x={x} y={y} dx={-4} dy={12} fontSize={14} fill="#9b9eac">
+        {payload.index + 1}
+      </text>
+    </g>
+  );
+}
+
 /* eslint-enable react/prop-types */
+
+/* -------------------------------------------------------------------------- */
+/*                                  COMPONENT                                 */
+/* -------------------------------------------------------------------------- */
 function Activity({ queryId }) {
   console.log(queryId);
   const [userActivityData, setActivityData] = useState([]);
@@ -60,16 +108,13 @@ function Activity({ queryId }) {
       barGap={-56}
       margin={[0, 0, 0, 0]}
     >
-      <text className="chart-title" x={0} y={15}>
-        Activité quotidienne
-      </text>
       <CartesianGrid vertical={false} strokeDasharray="3 3" stroke="#dedede" />
       <XAxis
         dataKey="day"
         axisLine={{ stroke: '#dedede' }}
         tickLine={false}
         tickMargin={10}
-        tick={{ color: '#9b9eac', fontSize: 14 }}
+        tick={<CustomAxisTick />}
       />
       <YAxis
         axisLine={false}
@@ -111,6 +156,10 @@ function Activity({ queryId }) {
     </BarChart>
   );
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                  PROPTYPE                                  */
+/* -------------------------------------------------------------------------- */
 
 Activity.propTypes = {
   queryId: PropTypes.string.isRequired,
