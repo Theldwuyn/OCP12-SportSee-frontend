235e202e56d4353889f1b7f7ed2b5c1031acbb7f
Guillaume <guillaumeargensse@hotmail.fr>
2024-12-23 11:13:42 +0100

    style: refactor style rules, add new variables

--- src/utils/variable.js
+++ src/utils/variable.js
@@ -1,3 +1,5 @@
 export const primaryColor = '#ff0101';
 export const secondaryColor = '#282d30';
 export const tooltipBg = 'rgba(196,196,196,0.5)';
+export const radarFill = 'rgba(255,1,1,0.7)';
+export const radialChartSecondaryColor = '#74798c';
